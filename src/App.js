1f20266e2411e4ab347fcd3455dfea6531b14436
Kevin Henry <kevinhenry@mac.com>
2021-05-06 19:29:12 -0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Carousel from 'react-bootstrap/Carousel';
+import axios from 'axios';
+
+
+
+
+
+
+componentDidMount() {
+  axios.get('http://localhost:3001/moods').then(response =>}
+    console.log(response.data)
+}
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1
   );
 }
 
